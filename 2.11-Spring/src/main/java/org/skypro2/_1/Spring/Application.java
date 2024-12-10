ae259c42a713c3ea5cb473599c0cefa9fe9c33d6
zhe-v <zhe-valentin@mail.ru>
2024-12-10 23:20:21 +0700

    ДЗ 2.11 Spring

---
+++ 2.11-Spring/src/main/java/org/skypro2/_1/Spring/Application.java
@@ -0,0 +1,13 @@
+package org.skypro2._1.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
